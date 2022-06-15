f4a9baf22dfa5d0de827f73aadebc98bb09add32
Wojciech Gajda <wojtekgajda94@gmail.com>
2022-06-15 23:46:50 +0200

    first component

---
+++ src/components/molecules/MenuItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+class MenuItem extends React.Component
+{
+    render() {
+        return (
+            <View style={[styles.menuItem, {backgroundColor: this.props.color}]}>
+                <Text>{this.props.text}</Text>
+            </View>
+        )
+    }
+}
+
+export default MenuItem
+
+const styles = StyleSheet.create({
+    menuItem: {
+      flex: 1,
+      margin: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 20
+    },
+  });
